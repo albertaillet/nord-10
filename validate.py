2ff535099c833c2b47d77546a633c9cfeef134dd
Albert Sund Aillet <albert@sundaillet.com>
2025-07-22 17:28:56 +0200

    validate types

--- validate.py
+++ validate.py
@@ -2,11 +2,14 @@
 import csv
 from pathlib import Path
 
+VALID_TYPES = {'MEM','REGBLOCK','CONV','ARG','REG','BIT','JUMP','SHIFT','TRANS','EXEC','INTER','SYS','WAIT','IO','IDENT'}
+
 instructions_path = Path(__file__).parent / 'instructions.csv'
 with instructions_path.open() as f:
     reader = csv.DictReader(f)
     for row in reader:
         assert row['code'], f'Invalid code {row["code"]}'
+        assert row['type'] in VALID_TYPES, f'Invalid type {row["type"]}'
         if row['binary']:
             assert len(row['binary']) == 16, f'Invalid binary {row["binary"]} for code {row["code"]}'
         if row['octal']:
