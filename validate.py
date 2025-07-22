27b70d25ae81166930ac94b606b73e9d8694db7d
Albert Sund Aillet <albert@sundaillet.com>
2025-07-22 19:21:46 +0200

    rename type to category

--- validate.py
+++ validate.py
@@ -2,18 +2,17 @@
 import csv
 from pathlib import Path
 
-VALID_TYPES = {'MEM','REGBLOCK','CONV','ARG','REG','BIT','JUMP','SHIFT','TRANS','EXEC','INTER','SYS','WAIT','IO','IDENT'}
+VALID_CATEGORIES = {'MEM','REGBLOCK','CONV','ARG','REG','BIT','JUMP','SHIFT','TRANS','EXEC','INTER','SYS','WAIT','IO','IDENT'}
 
 instructions_path = Path(__file__).parent / 'instructions.csv'
 with instructions_path.open() as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        assert row['code'], f'Invalid code {row["code"]}'
-        assert row['type'] in VALID_TYPES, f'Invalid type {row["type"]}'
-        if row['binary']:
-            assert len(row['binary']) == 16, f'Invalid binary {row["binary"]} for code {row["code"]}'
-        if row['octal']:
-            assert len(row['octal']) == 6, f'Invalid octal {row["octal"]} for code {row["code"]}'
-        if row['octal'] and row['binary']:
-            msg = f'Mismatch between octal {row["octal"]} and binary {row["binary"]} for code {row["code"]}'
-            assert int(row['octal'], 8) == int(row['binary'], 2), msg
+    f.readline()  # header
+    for binary, octal, mnemonic, category, _desc, _ref in csv.reader(f):
+        assert mnemonic, f'Invalid {mnemonic=}'
+        assert category in VALID_CATEGORIES, f'Invalid type {category=} for {mnemonic=}'
+        if octal:
+            assert int(octal, 8) < 2**16, f'Instruction {octal=} exceeds 16 bits.'
+            assert len(octal) == 6, f'Invalid {octal=} for {mnemonic=}'
+        if binary:
+            assert len(binary) == 16, f'Invalid {binary=} for {mnemonic=}'
+            assert int(octal, 8) == int(binary, 2), f'Mismatch between {octal=} and {binary=} for {mnemonic=}'
