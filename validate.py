c21e8d04a1514e1d59a4ce6bac7b746169311440
Albert Sund Aillet <albert@sundaillet.com>
2025-07-23 08:59:48 +0200

    use Category in validate

--- validate.py
+++ validate.py
@@ -2,14 +2,14 @@
 import csv
 from pathlib import Path
 
-VALID_CATEGORIES = {'MEM','REGBLOCK','CONV','ARG','REG','BIT','JUMP','SHIFT','TRANS','EXEC','INTER','SYS','WAIT','IO','IDENT'}
+from assembler import Category
 
 instructions_path = Path(__file__).parent / 'instructions.csv'
 with instructions_path.open() as f:
     f.readline()  # header
     for binary, octal, mnemonic, category, _impl, _desc, _ref in csv.reader(f):
         assert mnemonic, f'Invalid {mnemonic=}'
-        assert category in VALID_CATEGORIES, f'Invalid type {category=} for {mnemonic=}'
+        assert Category[category], f'Invalid type {category=} for {mnemonic=}'
         if octal:
             assert int(octal, 8) < 2**16, f'Instruction {octal=} exceeds 16 bits.'
             assert len(octal) == 6, f'Invalid {octal=} for {mnemonic=}'
